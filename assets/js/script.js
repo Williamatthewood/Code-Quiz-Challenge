50129baafd752dd0c2ee0953f81bd7118ef96fe2
Williamatthewood <w.wood@tcu.edu>
2023-03-15 13:14:53 -0500

    created to do list in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("test");
+/* To do list:
+
+set time variable
+create timer with set interval
+retrieve high scores from local storage and display in list items
+store high score into local storage
+create start button
+start button makes instructions disappear
+start button makes random question appear
+validate correct response and incorrect response
+    display correct/ incorrect after response
+    go to new question after response
+Create input box to put in initials after game ends
+Make incorrect guess subtract 5 seconds from the clock
+pause clock when game ends
+make score = time remaining
+create a reset high scores button
+
+*/
+
+var timeRemaining = 60;
