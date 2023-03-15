741d101d3dbb42a89fd71c746e3f3957880120ae
Williamatthewood <w.wood@tcu.edu>
2023-03-15 17:59:45 -0500

    started doing the question logic, but bloated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,15 @@ var highScoreList = document.getElementById("high-score-list");
 var timeLeftSpan = document.getElementById("time-left-span");
 var startButton = document.getElementById("start-button");
 var instructions = document.getElementById("instructions");
+var questionCard = document.getElementById("question-card");
+var questionText = document.getElementById("question-text");
+var answerChoices = document.getElementById("answer-choices");
+var validateAnswer = document.getElementById("validate-answer");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+
 
 var timerCount;
 var isPlaying = false;
@@ -44,7 +53,7 @@ function startGame(){
     startButton.disabled = true;
     startTimer();
     hideInstructions();
-    showQuestions();
+    question1();
 }
 
 function startTimer() {
@@ -63,13 +72,59 @@ function gameOver(){
 }
 
 function hideInstructions(){
-    instructions.setAttribute("style", "display: none");
-}
-
-function showQuestions(){
+    instructions.classList.add("hidden");
+    questionCard.classList.remove("hidden");
 
 }
 
+function question1(){
+    questionText.textContent = "This is Question 1. Answer it dummy!";
+    answer1.textContent = "This is answer 1";
+    answer1.classList.add("correct1");
+    answer2.textContent = "This is answer 2";
+    answer2.classList.add("incorrect1");
+    answer3.textContent = "This is answer 3";
+    answer3.classList.add("incorrect1");
+    answer4.textContent = "This is answer 4";
+    answer4.classList.add("incorrect1");
+    
+    
+}
+
+function question2(){
+    answer1.classList.remove("correct1");
+    answer2.classList.remove("incorrect1");
+    answer3.classList.remove("incorrect1");
+    answer4.classList.remove("incorrect1");
+
+    questionText.textContent = "This is Question 2. Answer it dummy!";
+    answer1.textContent = "This is answer 1";
+    answer1.classList.add("incorrect2");
+    answer2.textContent = "This is answer 2";
+    answer2.classList.add("correct2");
+    answer3.textContent = "This is answer 3";
+    answer3.classList.add("incorrect2");
+    answer4.textContent = "This is answer 4";
+    answer4.classList.add("incorrect2");
+    
+    
+}
+
+function checkAnswer(event){
+   console.log(event.target);
+
+   if(event.target.classList.contains("correct1")){
+    validateAnswer.textContent = "Correct!";
+    question2();
+   } else if(event.target.classList.contains("incorrect1")){
+    validateAnswer.textContent = "Incorrect!";
+    timerCount -= 5;
+    question2();
+   }
+
+}
+
+answerChoices.addEventListener("click", checkAnswer);
 startButton.addEventListener("click", startGame);
 
 init();
