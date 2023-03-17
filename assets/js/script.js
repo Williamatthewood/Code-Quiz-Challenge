d587c89b612059de4dc84b2a0ca67e57c2a4c8c8
Williamatthewood <w.wood@tcu.edu>
2023-03-17 11:45:33 -0500

    added descriptive comments to the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//get HTML elements by traversing the DOM
 var highScoreList = document.getElementById("high-score-list");
 var resetHighScoreButton = document.getElementById("reset");
 var timeLeftSpan = document.getElementById("time-left-span");
@@ -17,7 +18,7 @@ var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
 
-
+//variables used in JS
 var timerCount;
 var isPlaying = false;
 var timer;
@@ -26,22 +27,22 @@ var totalNumberOfQuestions = 5;
 var currentScore = 0;
 var scores = [];
 
-
+//function that runs right aways as the page loads
 function init(){
     getHighScores();
     timerCount = 60;
     timeLeftSpan.textContent = timerCount;
 }
-
+//pulls high scores from local storage and displays them in a list if any are stored
 function getHighScores(){
     var storedScores = JSON.parse(localStorage.getItem("scores"));
-
+//if no scores are stored, the function just returns
     if(storedScores !== null){
         scores = storedScores;
     } else {
         return;
     }
-
+//if there are more than 5 scores in storage, it removes them until there are 5
     if(scores.length > 5){
         for (let i = scores.length; i > 5; i--) {
             scores.pop();
@@ -56,7 +57,7 @@ function getHighScores(){
         highScoreList.appendChild(listItem);
     }
 }
-
+//takes the user's current score, adds it to the scores array and sorts it from greatest to smallest.
 function saveScore(){
     var newScore = {
         initials: initials.value,
@@ -72,20 +73,20 @@ function saveScore(){
     }
 
     console.log(scores);
-
+//stores the scores array into localstorage
     localStorage.setItem("scores", JSON.stringify(scores));
-    submitButton.classList.add("hidden");
-    initials.value = "";
+    submitButton.classList.add("hidden"); //hides the submit button, so user cannot submit more than 1 set of initials
+    initials.value = ""; //clears the initials field after the user submits
 
 }
-
+//clears the scores array and the high score list display when you press the Reset High Scores button
 function resetScores(){
     scores.splice(0);
     console.log(scores);
     localStorage.setItem("scores", JSON.stringify(scores));
     highScoreList.innerHTML = "";
 }
-
+//starts the timer and dynamically udpates the page when start button is clicked
 function startGame(){
     timerCount = 60;
     isPlaying = true;
@@ -94,7 +95,7 @@ function startGame(){
     hideInstructions();
     question1();
 }
-
+//uses setInterval to update the timer number, decremeting by 1 each second
 function startTimer() {
     timer = setInterval(function() {
         timerCount--;
@@ -105,7 +106,7 @@ function startTimer() {
         }
     }, 1000)
 }
-
+//when game is over, dynimically udpates the page and updates the users current score from the timer count
 function gameOver(){
     console.log("gameOver confirmed");
     isPlaying = false;
@@ -117,13 +118,13 @@ function gameOver(){
 }
 
 
-
+//hides instuctions and reveals questions
 function hideInstructions(){
     instructions.classList.add("hidden");
     questionCard.classList.remove("hidden");
 
 }
-
+//functions that update the text and correct answer for each question
 function question1(){
     questionText.textContent = "Which of the following is NOT a primitive data type in JavaScript?";
     answer1.textContent = "bit";
@@ -131,10 +132,7 @@ function question1(){
     answer2.textContent = "number";
     answer3.textContent = "string";
     answer4.textContent = "boolean";
-    
-    
 }
-
 function question2(){
     questionsAnswered++;
     answer1.classList.remove("correct");
@@ -144,11 +142,8 @@ function question2(){
     answer2.textContent = "<script>";
     answer2.classList.add("correct");
     answer3.textContent = "<link>";
-    answer4.textContent = "<head>";
-    
-    
+    answer4.textContent = "<head>";  
 }
-
 function question3(){
     questionsAnswered++;
     answer2.classList.remove("correct");
@@ -158,11 +153,8 @@ function question3(){
     answer2.textContent = "!=";
     answer3.textContent = "===";
     answer4.textContent = "==";
-    answer4.classList.add("correct");
-    
-    
+    answer4.classList.add("correct"); 
 }
-
 function question4(){
     questionsAnswered++;
     answer4.classList.remove("correct");
@@ -173,9 +165,6 @@ function question4(){
     answer2.textContent = ".sort()";
     answer3.textContent = ".add()";
     answer4.textContent = ".push()";
-
-    
-    
 }
 function question5(){
     questionsAnswered++;
@@ -187,10 +176,11 @@ function question5(){
     answer3.textContent = "The window object in the browser";
     answer3.classList.add("correct");
     answer4.textContent = "A variable called this";
-
 }
 
-
+//logic that checks if the button for the answer choice clicked has the "correct" class or not
+//using the event.target allows it to detect the specific button within the parent element "answerChoices"
+//uses the questionsAnswered counter to determine which question function to pull next
 function checkAnswer(event){
    var answerChoice = event.target;
    console.log(answerChoice);
@@ -244,11 +234,11 @@ function checkAnswer(event){
         }
     } 
 }
-
+//reloads the page to start the game over
 function replay(){
     location.reload();
 }
-
+//event listeners for all buttons on page
 startButton.addEventListener("click", startGame);
 answerChoices.addEventListener("click", checkAnswer);
 submitButton.addEventListener("click", function(event){
@@ -259,5 +249,5 @@ submitButton.addEventListener("click", function(event){
 resetHighScoreButton.addEventListener("click", resetScores);
 replayButton.addEventListener("click", replay);
 
-
+//start by calling the init function
 init();
